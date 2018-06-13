3ce80036ce2ee946e057dd39a35657c781b3d5fd
bboampong <baffouraduboampong@gmail.com>
2018-06-13 14:52:41 +0000

    Updated Service Woker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,31 +1,30 @@
 const cacheName = 'fileShare';
-const cacheVersion = `${cacheName}::1.1.0`;
+const cacheVersion = `${cacheName}::1.2.0`;
 
 const cachedFiles = [
     '/',
     '/css/styles.css',
     'images/bg.png',
-    'js/custom-file-input.j'
+    'js/custom-file-input.js'
 ];
 
+//Variables to hold Network files
 const networkFiles = [
 ];
 
+//Installing Service Worker
 self.addEventListener('install', event => {
-
     console.log('[pwa install]');
-
     event.waitUntil(
+        //Open Cache and all URLS to cache
         caches.open(cacheVersion)
             .then(cache => cache.addAll(cachedFiles))
     );
-
 });
 
+//Activating Service Worker
 self.addEventListener('activate', event => {
-
     console.log('[pwa activate]');
-
     event.waitUntil(
         caches.keys().then(keys =>
             Promise.all(
@@ -34,37 +33,26 @@ self.addEventListener('activate', event => {
             )
         )
     );
-
     return self.clients.claim();
-
 });
 
+//Fetch from cache or network
 self.addEventListener('fetch', event => {
-
+    //Fetch Network first, then cache
     if (networkFiles.filter(item => event.request.url.match(item)).length) {
-
         console.log('[network fetch]', event.request.url);
-
         event.respondWith(
             caches.match(event.request)
                 .then(response => response || fetch(event.request))
         );
-
     } else {
-
         console.log('[pwa fetch]', event.request.url);
-
         event.respondWith(
             caches.match(event.request)
                 .then(response => {
-
                     caches.open(cacheVersion).then(cache => cache.add(event.request.url));
-
                     return response || fetch(event.request);
-
                 })
         );
-
     }
-
 });
